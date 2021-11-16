a509f789e664d23ec19a5537b9f069df84f9fb2a
Felipe Matheus <fmatheusog@gmail.com>
2021-11-16 19:05:02 -0300

    Requisito 2 - User Input Validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,51 @@
 import { React, Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      loginButtonDisabled: true,
+    };
+  }
+
+  validateUserInput = () => {
+    const { username } = this.state;
+    const inputMinLen = 3;
+
+    if (username.length > inputMinLen) {
+      this.setState({
+        loginButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        loginButtonDisabled: true,
+      });
+    }
+  }
+
+  handleUserInputChange = (ev) => {
+    this.setState({
+      username: ev.target.value,
+    }, () => this.validateUserInput());
+  }
+
   render() {
+    const { username, loginButtonDisabled } = this.state;
+
     return (
       <div data-testid="page-login">
-        Teste
+        Username:
+        <input
+          type="text"
+          name="username"
+          id="username"
+          data-testid="login-name-input"
+          onChange={ this.handleUserInputChange }
+          value={ username }
+        />
+        <button type="submit" disabled={ loginButtonDisabled }>Entrar</button>
       </div>
     );
   }
